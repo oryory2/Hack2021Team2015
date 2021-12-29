b456112f0705dc54f8315ff0052c2fc7f08f9a76
oryory2 <oryory2@gmail.com>
2021-12-29 14:59:21 +0200

    almost done (:!

--- ServerSide.py
+++ ServerSide.py
@@ -5,6 +5,7 @@ import socket
 import sys
 from threading import Thread
 
+# Text colors
 blueColor = '\033[96m'  # blue
 yellowColor = '\033[92m'  # yellow
 failColor = '\033[96m'  # red
@@ -22,8 +23,6 @@ class Server:
         self.teamTwoName = None
         self.teamOneSocket = None
         self.teamTwoSocket = None
-        self.teamOneAddress = None
-        self.teamTwoAddress = None
         self.answerOne = None
         self.answerTwo = None
         self.answer = None
@@ -39,7 +38,9 @@ class Server:
         tcpPortNumberInBytes = self.tPortNumber.to_bytes(byteorder='big', length=2)
         self.broadMsg = magicCookieInBytes + messageTypeInBytes + tcpPortNumberInBytes
 
-        # Initializing the UDP Socket
+        # Initializing the different sockets
+
+        # UDP Socket
         try:
             self.udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
             self.udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -53,17 +54,20 @@ class Server:
             print(failColor + "Hostname server couldn't be resolved")
             sys.exit()
 
-        # Initializing the TCP Socket
+        # TCP Socket
         try:
             self.tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except:
             print(failColor + "Failed to create server TCP socket")
             sys.exit()
+
+        # Bind the tcp socket with the server host/port
         self.tcpSocket.bind((self.host, self.tPortNumber))
 
 
 
-    def acceptingClients(self):  # First function to run on the server side
+    # First function to run on the server side
+    def acceptingClients(self):
 
         self.tcpSocket.listen(2)
 
@@ -77,54 +81,30 @@ class Server:
         # Wait for two Clients to connect the Server
         while self.clientsConnected < 2:
             if self.clientsConnected == 0:
-                self.teamOneSocket, self.teamOneAddress = self.tcpSocket.accept()  # Wait for the first Client to connect
-                self.teamOneName = self.teamOneSocket.recv(1024).decode('UTF-8')  # receive the TeamName of the first Team
+                self.teamOneSocket, addressOne = self.tcpSocket.accept()  # Wait for the first Client to connect
+                self.teamOneName = self.teamOneSocket.recv(1024).decode(
+                    'UTF-8')  # receive the TeamName of the first Team
                 self.clientsConnected = self.clientsConnected + 1
                 print(yellowColor + "First client connected..")
             else:
-                self.teamTwoSocket, self.teamTwoAddress = self.tcpSocket.accept()  # Wait for the second Client to connect
-                self.teamTwoName = self.teamTwoSocket.recv(1024).decode('UTF-8')  # receive the TeamName of the second Team
+                self.teamTwoSocket, addressTwo = self.tcpSocket.accept()  # Wait for the second Client to connect
+                self.teamTwoName = self.teamTwoSocket.recv(1024).decode(
+                    'UTF-8')  # receive the TeamName of the second Team
                 self.clientsConnected = self.clientsConnected + 1
                 print(yellowColor + "Second client connected..")
         self.startTheGame()
 
 
 
-    def broadcastMsg(self):  # Function to send the broadcastMsg via UDP transport
+    # Function to send the broadcastMsg via UDP transport
+    def broadcastMsg(self):
         while self.clientsConnected != 2:
             self.udpSocket.sendto(self.broadMsg, ('255.255.255.255', self.uPortNumber))
             time.sleep(1)  # Wait one second between messages
 
 
 
-    def checkConnection(self):  # Function for handling Clients that disconnect from the server
-        oneDisconnected = False
-        TwoDisconnected = False
-
-        try:
-            self.teamOneSocket.sendall("".encode())
-        except:
-            oneDisconnected = True
-        try:
-            self.teamTwoSocket.sendall("".encode())
-        except:
-            TwoDisconnected = True
-
-
-        if oneDisconnected and TwoDisconnected:  # Both Clients disconnected from the server
-            print(failColor + "Both Teams disconnected, restarting Server..")
-            self.closeSocketsAndRestart()
-
-        if oneDisconnected:
-            print(failColor + "Team " + str(self.teamOneName) + " has disconnected from the server, Team " + str(self.teamTwoName) + " won the match")
-            self.closeSocketsAndRestart()
-
-        if TwoDisconnected:
-            print(failColor + "Team " + str(self.teamTwoName) + " has disconnected from the server, Team " + str(self.teamOneName) + " won the match")
-            self.closeSocketsAndRestart()
-
-
-
+    # Function that runs after the two Clients connected to the Server
     def startTheGame(self):
 
         time.sleep(10)  # wait 10 seconds until the start of the game
@@ -153,10 +133,17 @@ class Server:
             mathMsg = "How much is " + str(numOne) + " - " + str(numTwo) + "?"
             result = numOne - numTwo
 
-
         # Welcome the two Teams and ask the math question
-        self.teamOneSocket.sendall(bytes("Welcome to the tournament of BGU Quick Maths.. get ready, the game is going to begin shortly..\n""Teams: \n1. " + str(self.teamOneName) + "\n2. " + str(self.teamTwoName) + "\n ====== \nPlease answer the following question as fast as you can:\n" + mathMsg, 'UTF-8'))
-        self.teamTwoSocket.sendall(bytes("Welcome to the tournament of BGU Quick Maths.. get ready, the game is going to begin shortly..\n""Teams: \n1. " + str(self.teamOneName) + "\n2. " + str(self.teamTwoName) + "\n ====== \nPlease answer the following question as fast as you can:\n" + mathMsg, 'UTF-8'))
+        self.teamOneSocket.sendall(bytes(
+            "Welcome to the tournament of BGU Quick Maths.. get ready, the game is going to begin shortly..\n""Teams: \n1. " + str(
+                self.teamOneName) + "\n2. " + str(
+                self.teamTwoName) + "\n ====== \nPlease answer the following question as fast as you can:\n" + mathMsg,
+            'UTF-8'))
+        self.teamTwoSocket.sendall(bytes(
+            "Welcome to the tournament of BGU Quick Maths.. get ready, the game is going to begin shortly..\n""Teams: \n1. " + str(
+                self.teamOneName) + "\n2. " + str(
+                self.teamTwoName) + "\n ====== \nPlease answer the following question as fast as you can:\n" + mathMsg,
+            'UTF-8'))
 
         # Creating two Threads that will take answer from the two Clients
         teamOneGameThread = Thread(target=self.getAnswerFromTeam, args=(self.teamOneSocket, 1,))
@@ -167,7 +154,6 @@ class Server:
 
         teamOneGameThread.join()
         teamTwoGameThread.join()
-
 
         #  Check the connection with the Clients
         self.checkConnection()
@@ -209,6 +195,137 @@ class Server:
 
 
 
+    # Function that getting an answer from each client (runs two times in parallel (two threads))
+    def getAnswerFromTeam(self, teamSocket, teamNumber):
+
+        teamSocket.setblocking(False)
+        t0 = datetime.now()
+
+        while (
+                datetime.now() - t0).seconds <= 10 and self.answer is None:  # While none of both teams has answered, and 10 second didn't pass yet
+            try:
+                teamAnswer = teamSocket.recv(1024)
+            except:
+                continue
+
+            if teamAnswer != None:  # The team has answered
+                if teamNumber == 1:  # teamOne has answered
+                    self.answerOne = teamAnswer
+                    self.answer = teamAnswer
+                    return
+                if teamNumber == 2:  # teamTwo has answered
+                    self.answerTwo = teamAnswer
+                    self.answer = teamAnswer
+                    return
+
+
+
+    def printResultWin(self, winnerTeam, result):  # Print the result of the match (not Draw)
+
+        print(blueColor + "Game Over!\nThe correct answer was " + str(
+            result) + "!\nCongratulations to the winner: " + winnerTeam)
+        self.teamOneSocket.sendall(bytes(
+            "Game Over!\nThe correct answer was " + str(result) + "!\nCongratulations to the winner: " + str(
+                winnerTeam), 'UTF-8'))
+        self.teamTwoSocket.sendall(bytes(
+            "Game Over!\nThe correct answer was " + str(result) + "!\nCongratulations to the winner: " + str(
+                winnerTeam), 'UTF-8'))
+
+
+
+    def printResultDraw(self, result):  # Print the result of the match (Draw)
+
+        print(blueColor + "Game Over!\nThe correct answer was " + str(
+            result) + "!\nNone of the team answered on time, so there is a Draw!")
+        self.teamOneSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(
+            result) + "!\nNone of the team answered on time, so there is a Draw!", 'UTF-8'))
+        self.teamTwoSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(
+            result) + "!\nNone of the team answered on time, so there is a Draw!", 'UTF-8'))
+
+
+
+    # Function that updating the TeamsTable in case of a win of one Team
+    def updateTeamsTable_win(self, winnerTeam, loserTeam):
+        if winnerTeam in self.teamsTable:
+            self.teamsTable[winnerTeam] = (
+                self.teamsTable[winnerTeam][0] + 1, self.teamsTable[winnerTeam][1] + 1)  # (gamesPlayed, gamesWon)
+        else:
+            self.teamsTable[winnerTeam] = (1, 1)
+        if loserTeam in self.teamsTable:
+            self.teamsTable[loserTeam] = (
+                self.teamsTable[loserTeam][0] + 1, self.teamsTable[loserTeam][1])  # (gamesPlayed, gamesWon)
+        else:
+            self.teamsTable[loserTeam] = (1, 0)
+
+
+
+    # Function that updating the TeamsTable in case of a draw between the teams
+    def updateTeamsTable_draw(self):
+        if self.teamOneName in self.teamsTable:
+            self.teamsTable[self.teamOneName] = (
+                self.teamsTable[self.teamOneName][0] + 1,
+                self.teamsTable[self.teamOneName][1])  # (gamesPlayed, gamesWon)
+        else:
+            self.teamsTable[self.teamOneName] = (1, 0)
+        if self.teamTwoName in self.teamsTable:
+            self.teamsTable[self.teamTwoName] = (
+                self.teamsTable[self.teamTwoName][0] + 1,
+                self.teamsTable[self.teamTwoName][1])  # (gamesPlayed, gamesWon)
+        else:
+            self.teamsTable[self.teamTwoName] = (1, 0)
+
+
+
+    # Function that shows the best three teams played on the server until now (by number of wins)
+    def showBestTeams(self):
+
+        sortedLst = sorted(self.teamsTable.items(), key=lambda tup: tup[1][1], reverse=True)
+        if len(sortedLst) == 1:
+            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(
+                sortedLst[0][1][1]))
+
+        elif len(sortedLst) == 2:
+            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(
+                sortedLst[0][1][1]) + "\n2. " +
+                  str(sortedLst[1][0]) + " - wins: " + str(sortedLst[1][1][1]))
+        else:
+            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(
+                sortedLst[0][1][1]) + "\n2. " + str(sortedLst[1][0]) + " - wins: " + str(
+                sortedLst[1][1][1]) + "\n3. " + str(sortedLst[2][0]) + " - wins: " + str(sortedLst[2][1][1]))
+
+
+
+    # Function for handling Clients that disconnect from the server
+    def checkConnection(self):
+        oneDisconnected = False
+        TwoDisconnected = False
+
+        try:
+            self.teamOneSocket.sendall("".encode())
+        except:
+            oneDisconnected = True
+        try:
+            self.teamTwoSocket.sendall("".encode())
+        except:
+            TwoDisconnected = True
+
+        if oneDisconnected and TwoDisconnected:  # Both Clients disconnected from the server
+            print(failColor + "Both Teams disconnected, restarting Server..")
+            self.closeSocketsAndRestart()
+
+        if oneDisconnected:
+            print(failColor + "Team " + str(self.teamOneName) + " has disconnected from the server, Team " + str(
+                self.teamTwoName) + " won the match")
+            self.closeSocketsAndRestart()
+
+        if TwoDisconnected:
+            print(failColor + "Team " + str(self.teamTwoName) + " has disconnected from the server, Team " + str(
+                self.teamOneName) + " won the match")
+            self.closeSocketsAndRestart()
+
+
+
+    # Function for handling the server state in case of disconnected client
     def closeSocketsAndRestart(self):
 
         # Closing the first tcpSocket
@@ -231,83 +348,8 @@ class Server:
         self.restartServer()
 
 
-    def updateTeamsTable_win(self, winnerTeam, loserTeam):
-        if winnerTeam in self.teamsTable:
-            self.teamsTable[winnerTeam] = (self.teamsTable[winnerTeam][0] + 1, self.teamsTable[winnerTeam][1] + 1)  # (gamesPlayed, gamesWon)
-        else:
-            self.teamsTable[winnerTeam] = (1, 1)
-        if loserTeam in self.teamsTable:
-            self.teamsTable[loserTeam] = (self.teamsTable[loserTeam][0] + 1, self.teamsTable[loserTeam][1])  # (gamesPlayed, gamesWon)
-        else:
-            self.teamsTable[loserTeam] = (1, 0)
 
-
-    def updateTeamsTable_draw(self):
-        if self.teamOneName in self.teamsTable:
-            self.teamsTable[self.teamOneName] = (self.teamsTable[self.teamOneName][0] + 1, self.teamsTable[self.teamOneName][1])  # (gamesPlayed, gamesWon)
-        else:
-            self.teamsTable[self.teamOneName] = (1, 0)
-        if self.teamTwoName in self.teamsTable:
-            self.teamsTable[self.teamTwoName] = (self.teamsTable[self.teamTwoName][0] + 1, self.teamsTable[self.teamTwoName][1])  # (gamesPlayed, gamesWon)
-        else:
-            self.teamsTable[self.teamTwoName] = (1, 0)
-
-
-    def printResultWin(self, winnerTeam, result):  # not Draw
-
-        print(blueColor + "Game Over!\nThe correct answer was " + str(result) + "!\nCongratulations to the winner: " + winnerTeam)
-        self.teamOneSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(result) + "!\nCongratulations to the winner: " + str(winnerTeam), 'UTF-8'))
-        self.teamTwoSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(result) + "!\nCongratulations to the winner: " + str(winnerTeam), 'UTF-8'))
-
-
-
-    def printResultDraw(self, result):  # Draw
-
-        print(blueColor + "Game Over!\nThe correct answer was " + str(result) + "!\nNone of the team answered on time, so there is a Draw!")
-        self.teamOneSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(result) + "!\nNone of the team answered on time, so there is a Draw!", 'UTF-8'))
-        self.teamTwoSocket.sendall(bytes("Game Over!\nThe correct answer was " + str(result) + "!\nNone of the team answered on time, so there is a Draw!", 'UTF-8'))
-
-
-
-    def getAnswerFromTeam(self, teamSocket, teamNumber):
-
-        teamSocket.setblocking(False)
-        t0 = datetime.now()
-
-        while (datetime.now() - t0).seconds <= 10 and self.answer is None:  # While none of both teams has answered, and 10 second didn't pass yet
-            try:
-                teamAnswer = teamSocket.recv(1024)
-            except:
-                continue
-
-            if teamAnswer != None:  # The team has answered
-                if teamNumber == 1:  # teamOne has answered
-                    self.answerOne = teamAnswer
-                    self.answer = teamAnswer
-                    return
-                if teamNumber == 2:  # teamTwo has answered
-                    self.answerTwo = teamAnswer
-                    self.answer = teamAnswer
-                    return
-
-
-
-    # Function that shows the best three teams played on the server until now
-    def showBestTeams(self):
-
-        sortedLst = sorted(self.teamsTable.items(), key=lambda tup: tup[1][1], reverse=True)
-        if len(sortedLst) == 1:
-            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(sortedLst[0][1][1]))
-
-        elif len(sortedLst) == 2:
-            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(sortedLst[0][1][1]) + "\n2. " +
-            str(sortedLst[1][0]) + " - wins: " + str(sortedLst[1][1][1]))
-        else:
-            print(yellowColor + "The best teams on the server are:\n1. " + str(sortedLst[0][0]) + " - wins: " + str(
-                sortedLst[0][1][1]) + "\n2. " + str(sortedLst[1][0]) + " - wins: " + str(sortedLst[1][1][1]) + "\n3. " + str(sortedLst[2][0]) + " - wins: " + str(sortedLst[2][1][1]))
-
-
-
+    # Function for restarting the server
     def restartServer(self):
 
         print(blueColor + "Game over, sending out offer requests...")
@@ -317,8 +359,6 @@ class Server:
         self.teamTwoName = None
         self.teamOneSocket = None
         self.teamTwoSocket = None
-        self.teamOneAddress = None
-        self.teamTwoAddress = None
         self.answerOne = None
         self.answerTwo = None
         self.answer = None
@@ -344,9 +384,11 @@ class Server:
         else:
             self.acceptingClients()  # Continue in running the server
 
-    def stopTheGameFunc(self):  # Function that stops the Client
-        self.stopTheGame = True
 
+
+    # Function that stops the Server
+    def stopTheGameFunc(self):
+        self.stopTheGame = True
 
 
 if __name__ == "__main__":
